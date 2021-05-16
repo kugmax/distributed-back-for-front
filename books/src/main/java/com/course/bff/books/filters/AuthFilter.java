37f1882d9714696d7db79ee5cbc72a67b5a32bfb
Maxim Kugatov <maximkugatov@gmail.com>
2021-05-16 18:41:41 +0300

    feat: added AuthFilter

---
+++ books/src/main/java/com/course/bff/books/filters/AuthFilter.java
@@ -0,0 +1,28 @@
+package com.course.bff.books.filters;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class AuthFilter implements Filter {
+    private static final String ACCESS_TOKEN_VALUE = "YWxhZGRpbjpvcGVuc2VzYW1l";
+    private static final String ACCESS_TOKEN_HEADER = "Authorization";
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+
+        if (!ACCESS_TOKEN_VALUE.equals(request.getHeader(ACCESS_TOKEN_HEADER))) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        filterChain.doFilter(request, servletResponse);
+    }
+}
