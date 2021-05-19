31775524159f835bb809808f4a17bbe2c691dc68
Maxim Kugatov <maximkugatov@gmail.com>
2021-05-19 11:27:26 +0300

    chore: init FrontApp

---
+++ FrontApp/src/main/java/com/kugmax/learn/frontapp/FrontApp/FrontAppApplication.java
@@ -0,0 +1,13 @@
+package com.kugmax.learn.frontapp.FrontApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontAppApplication.class, args);
+	}
+
+}
